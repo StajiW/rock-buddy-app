1ba77ea6aedc488abccf819364855fbfc0f131a7
StajiW <stijnjw11@gmail.com>
2023-08-31 13:34:30 +0200

    number display fail

--- electron-vite-vue/src/components/NumberDisplay.vue
+++ electron-vite-vue/src/components/NumberDisplay.vue
@@ -1,5 +1,7 @@
 <script setup lang='ts'>
-defineProps({
+import { watch, ref, Ref, onMounted, reactive, onUpdated } from 'vue'
+
+const props = defineProps({
     str: {
         type: String,
         required: true
@@ -11,14 +13,60 @@ defineProps({
 })
 
 const CHARACTERS = ' 0123456789.%'
+const SCROLL_DURATION = 200
+
+enum Animation {
+    Up,
+    Down,
+    None
+}
+
+type Character = {
+    digits: [string, string, string]    // 0 is above, 1 is shown, 2 is below
+    animation: Animation
+}
+
+let characters = reactive<Character[]>([])
+
+watch(() => props.str, (newStr, oldStr) => {
+    for (let i = 0; i < newStr.length; i++) {
+        const oldIndex = CHARACTERS.indexOf(oldStr[i])
+        const newIndex = CHARACTERS.indexOf(newStr[i])
+
+        if (newIndex === oldIndex) continue
+        if (newIndex < oldIndex) {
+            characters[i].digits[0] = newStr[i]
+            characters[i].animation = Animation.Down
+
+            setTimeout(() => {
+                characters[i].digits[1] = newStr[i]
+                characters[i].animation = Animation.None
+            }, SCROLL_DURATION - 20)
+        }
+        else {
+            characters[i].digits[2] = newStr[i]
+            characters[i].animation = Animation.Up
+
+            setTimeout(() => {
+                characters[i].digits[1] = newStr[i]
+                characters[i].animation = Animation.None
+            }, SCROLL_DURATION - 20)
+        }
+    }
+})
+
+onMounted(() => {
+    characters = reactive(Array(props.stringLength).fill(undefined).map(_ => { return {
+        digits: ['0', '0', '0'],
+        animation: Animation.None
+    }}))
+})
 </script>
 
 <template>
 <div class='NumberDisplay'>
-    <div class='Character'
-    v-for='i in stringLength'
-    :style="{ top: `${-100 * (CHARACTERS.indexOf(str[i - 1]))}%` }">
-        <div class='Digit' v-for='j in CHARACTERS'>{{ j }}</div>    
+    <div class='Character' v-for='character in characters' :class='Animation[character.animation]' >
+        <div class='Digit' v-for='digit in character.digits'>{{ digit }}</div>    
     </div>
 </div>
 </template>
@@ -35,14 +83,32 @@ const CHARACTERS = ' 0123456789.%'
     font-size: 1rem;
 
     overflow: hidden;
+
+    /* border: 1px solid black; */
 }
 
 .Character {
     position: relative;
     display: inline-block;
+    top: -100%;
+}
 
-    transform: top;
-    transition-duration: .3s;
+.Character.Up {
+    animation: Up 200ms;
+}
+
+@keyframes Up {
+    from { top: -100% }
+    to { top: -200% }
+}
+
+.Character.Down {
+    animation: Down 200ms;
+}
+
+@keyframes Down {
+    from { top: -100% }
+    to { top: 0% }
 }
 
 .Digit {
--- electron-vite-vue/src/pages/Main.vue
+++ electron-vite-vue/src/pages/Main.vue
@@ -4,14 +4,20 @@ import CurrentSong from '../components/CurrentSong.vue'
 import InGame from '../components/InGame.vue'
 import Leaderboard from '../components/Leaderboard.vue'
 import RockSniffer, { SnifferState } from '../scripts/rocksniffer'
+import NumberDisplay from '../components/NumberDisplay.vue'
 
 const rockSniffer = RockSniffer.instance
 
 const snifferState: Ref<SnifferState> = ref(rockSniffer.state)
+const number: Ref<string> = ref('0000')
 
 rockSniffer.on('stateChange', (state: SnifferState) => {
     snifferState.value = state
 })
+
+setInterval(() => {
+    number.value = Math.floor(Math.random() * 10000).toString().padStart(4, '0')
+}, 2000)
 </script>
 
 <template>
@@ -23,10 +29,19 @@ rockSniffer.on('stateChange', (state: SnifferState) => {
     <InGame      v-if='snifferState === SnifferState.InSong'/>
     <Leaderboard v-if='snifferState === SnifferState.InMenu'/>
 </div>
-
+<!-- <NumberDisplay :str='number' :string-length='4' /> -->
 </template>
 
 <style scoped>
+.NumberDisplay {
+    height: 3rem;
+    font-size: 3rem;
+}
+
+.NumberDisplay:deep(.Digit) {
+    height: 3rem;
+}
+
 #currentSong {
     margin-bottom: 1rem;
 }
